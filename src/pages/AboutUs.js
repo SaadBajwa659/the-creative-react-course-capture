fedddf193a3af489cb922a9412eb8b1ee5921890
Saad bajwa <saadbajwa659@gmail.com>
2024-01-26 15:26:29 +0500

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 import React from "react";
 // Page Components
 import AbouSection from '../components/AboutSection.js';
+import ServicesSection from "../components/ServicesSection.js";
 
 const AboutUs = () => {
     return (
-        <AbouSection />
+        <>
+            <AbouSection />
+            <ServicesSection />
+        </>
     )
 }
 
