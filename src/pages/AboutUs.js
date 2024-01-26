18facb1c17ea99ba802c5a6db25de3f4538f66b3
Saad bajwa <saadbajwa659@gmail.com>
2024-01-26 15:35:10 +0500

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 // Page Components
 import AbouSection from '../components/AboutSection.js';
 import ServicesSection from "../components/ServicesSection.js";
+import FaqSection from "../components/FaqSection.js";
 
 const AboutUs = () => {
     return (
         <>
             <AbouSection />
             <ServicesSection />
+            <FaqSection />
         </>
     )
 }
