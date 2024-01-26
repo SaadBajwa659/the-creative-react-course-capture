0c4dad6a067d3324a18b9bd2306484d726e13ecd
Saad bajwa <saadbajwa659@gmail.com>
2024-01-26 15:06:15 +0500

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+// Page Components
+import AbouSection from '../components/AboutSection.js';
+
+const AboutUs = () => {
+    return (
+        <AbouSection />
+    )
+}
+
+export default AboutUs;
