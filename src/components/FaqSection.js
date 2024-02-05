7e74d0cc1d52a63630224f63e5c3ca8586d085f5
Saad bajwa <saadbajwa659@gmail.com>
2024-02-05 19:12:14 +0500

    18. Finishing Faq Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import { LayoutGroup } from "framer-motion";
 
 const FaqSection = () => {
     return (
@@ -9,48 +10,40 @@ const FaqSection = () => {
             <h2>
                 Any Questions <span>FAQ</span>
             </h2>
-            <Toggle>
-                <div className="question">
-                    <h4>How Do I Start?</h4>
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus suscipit commodi magnam sunt hic. Neque atque veniam quidem recusandae magni.
-                        </p>
-                    </div>
-                    <div className="faq-line"></div>
-                </div>
-            </Toggle>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corporis.
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corporis.
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What Products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corporis.
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <LayoutGroup>
+                <Toggle title="How Do I Start?">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus suscipit commodi magnam sunt hic. Neque atque veniam quidem recusandae magni.
+                            </p>
+                        </div>
+                </Toggle>
+                <Toggle title="Daily Schedule">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corporis.
+                            </p>
+                        </div>
+                </Toggle>
+                <Toggle title="Different Payment Methods">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corporis.
+                            </p>
+                        </div>
+                </Toggle>
+                <Toggle title="What Products do you offer?">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corporis.
+                            </p>
+                        </div>
+                </Toggle>
+            </LayoutGroup>
         </Faq>
     );
 }
